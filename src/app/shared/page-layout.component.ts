e97bc530272a5d8cea91f010903b763fcb0aa6d3
daniel.granados <daniel.granados@sofka.com.co>
2023-05-18 15:35:14 -0500

    Add: home image and css

--- src/app/shared/page-layout.component.ts
+++ src/app/shared/page-layout.component.ts
@@ -4,11 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-page-layout',
   template: `
     <div class="page-layout">
-      <app-nav-bar></app-nav-bar>
+      <app-nav-bar class="nav-bar"></app-nav-bar>
       <div class="page-layout__content">
         <ng-content></ng-content>
       </div>
     </div>
   `,
+  styles: [
+    `
+      .page-layout {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        height: 100%;
+        width: 100%;
+      }
+      .nav-bar {
+        width: 100%;
+        height: 20vh;
+      }
+    `,
+  ],
 })
 export class PageLayoutComponent {}
