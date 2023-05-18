1b7a9ebd47df77efa8968b7bbbc4ae02032c4716
daniel.granados <daniel.granados@sofka.com.co>
2023-05-18 09:48:38 -0500

    add: auth0 config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,23 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: environment.auth0.authorizationParams.redirect_uri,
+      },
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
